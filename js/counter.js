283af94b59c339356704e3b393ca6e1a07ad6e59
Shivam020202 <charlieshivam70@gmail.com>
2025-05-27 18:39:45 +0530

    contact page added and content updated

--- js/counter.js
+++ js/counter.js
@@ -9,10 +9,10 @@ function isElementInViewport($el) {
 $(window).on("scroll", function () {
   if (isElementInViewport($(".odometer"))) {
     setTimeout(function () {
-      $(".style-1-1").html(20);
-      $(".style-1-2").html(1.8);
-      $(".style-1-3").html(1800);
-      $(".style-1-4").html(3);
+      $(".style-1-1").html(10000);
+      $(".style-1-2").html(1);
+      $(".style-1-3").html(80);
+      $(".style-1-4").html(100);
     }, 0);
   }
 });
