8f36a6c692335a5c2e86381231ec8b3f58271e85
Shivam020202 <charlieshivam70@gmail.com>
2025-05-31 18:07:39 +0530

    changes done

--- js/counter.js
+++ js/counter.js
@@ -9,10 +9,10 @@ function isElementInViewport($el) {
 $(window).on("scroll", function () {
   if (isElementInViewport($(".odometer"))) {
     setTimeout(function () {
-      $(".style-1-1").html(10000);
+      $(".style-1-1").html(30);
       $(".style-1-2").html(1);
-      $(".style-1-3").html(80);
-      $(".style-1-4").html(100);
+      $(".style-1-3").html(83);
+      $(".style-1-4").html(3);
     }, 0);
   }
 });
